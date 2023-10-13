60cb67f0c21f461c9732c0505589e81583c5dfae
yeaeunii <dpdmsdl4543@gmail.com>
2023-10-13 09:09:04 +0900

    예은 boardwirte 화면2

--- app/src/main/java/com/example/shine/BoardWriteActivity.kt
+++ app/src/main/java/com/example/shine/BoardWriteActivity.kt
@@ -7,5 +7,17 @@ class BoardWriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_write)
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/shine/post_fm.kt
+++ app/src/main/java/com/example/shine/post_fm.kt
@@ -1,10 +1,12 @@
 package com.example.shine
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class post_fm : Fragment() {
@@ -15,6 +17,16 @@ class post_fm : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var postV = inflater.inflate(R.layout.fragment_post_fm, container, false)
+
+        var btnWrite : Button = postV.findViewById(R.id.btnWrite)
+
+        btnWrite.setOnClickListener {
+                                //fragment 올려진 activity정보 가져오기  -> context 이용
+            val intent = Intent(context, BoardWriteActivity::class.java)
+            startActivity(intent)
+        }
+
+
         return postV
     }
 
